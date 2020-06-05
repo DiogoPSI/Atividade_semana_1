4eeafed5cf8818a926582b9eecb992f1114c8463
DiogoPSI <diogo.2002.miguel@gmail.com>
2020-06-05 18:43:58 +0100

    atividade semana 1

---
+++ app1.py
@@ -0,0 +1,23 @@
+from math import *
+nome = "João"
+idade = 35
+# strings
+# print("Existia um homem chamado " + nome)
+# print("Ele tinha " + idade + " anos")
+# print(nome.index("j")) mostra a posicao de um caracter numa string
+# print(nome.replace("o", "i"))
+# print(nome.upper())
+# print(nome.lower())
+# print(nome.isupper())
+# print(nome.islower())
+# numeros
+# print(str(idade) + " anos") converte para string
+# pow(3, 2) calcular o expoente
+# max(3, 2) obter maior valor
+# min(3, 2) obter menor valor
+# round(3.6) arredonda um numero
+# floor(3.7) arredonda um numero para baixo
+# ceil(3.7) arredonda um numero para cima
+# print(sqrt(36))
+nome = input("Introduza o seu nome: ")
+print("Olá " + nome + "!")
---
+++ calculadora.py
@@ -0,0 +1,6 @@
+num1 = input("Introduza um numero: ")
+num2 = input("Introduza outro numero: ")
+resultado = float(num1) + float(num2)
+
+print(resultado)
+
